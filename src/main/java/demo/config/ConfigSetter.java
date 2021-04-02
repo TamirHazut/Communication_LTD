634ba731120e54e0eef550de639384c306d2ea79
Tamir_Hazut <tamirhazut1994@gmail.com>
2021-04-02 19:25:26 +0300

    Enable SSL/TLSv1.2

--- src/main/java/demo/config/ConfigSetter.java
+++ src/main/java/demo/config/ConfigSetter.java
@@ -6,7 +6,10 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.Http2;
 import org.springframework.boot.web.server.Ssl;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,33 +18,55 @@ import lombok.AllArgsConstructor;
 
 @Configuration
 @AllArgsConstructor(onConstructor = @__(@Autowired))
-public class ConfigSetter {
+public class ConfigSetter implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 	private XMLReader xmlReader;
 
-    @Bean
-    public DataSource getDataSource() {
-    	Configurations configurations = xmlReader.loadConfigFile();
-    	Map<Permission, Object> permissions = configurations.getConfigurations(Permission.DB.getId());
-    	DBConfig dbConfig = (DBConfig) permissions.get(Permission.DB);
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://" + dbConfig.getIp() + ":" + dbConfig.getPort()+"/" + dbConfig.getDatabase());
-        dataSourceBuilder.username(dbConfig.getUser());
-        dataSourceBuilder.password(dbConfig.getPassword());
-        return dataSourceBuilder.build();
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+		Configurations configurations = xmlReader.loadConfigFile();
+		Map<Permission, Object> permissions = configurations.getConfigurations(Permission.SSL.getId());
+		SSLConfig sslConfig = (SSLConfig) permissions.get(Permission.SSL);
+        factory.setContextPath("");
+        factory.setPort(sslConfig.getPort());
+        factory.setSsl(getSSL());
+        factory.setHttp2(getHttp2());
     }
     
-    @Bean
-    public Ssl getSSL() {
-       	Configurations configurations = xmlReader.loadConfigFile();
-    	Map<Permission, Object> permissions = configurations.getConfigurations(Permission.SSL.getId());
-    	SSLConfig sslConfig = (SSLConfig) permissions.get(Permission.SSL);
-    	Ssl ssl = new Ssl();
-    	ssl.setKeyStoreType(sslConfig.getKeyStoreType());
-    	ssl.setKeyStore(sslConfig.getKeyStore());
-    	ssl.setKeyStorePassword(sslConfig.getKeyStorePassword());
-    	ssl.setKeyAlias(sslConfig.getKeyAlias());
-    	return ssl;
-    	
+    public Http2 getHttp2() {
+    	Http2 http2Config = new Http2();
+    	http2Config.setEnabled(true);
+    	return http2Config;
     }
+	
+	public Ssl getSSL() {
+		Configurations configurations = xmlReader.loadConfigFile();
+		Map<Permission, Object> permissions = configurations.getConfigurations(Permission.SSL.getId());
+		SSLConfig sslConfig = (SSLConfig) permissions.get(Permission.SSL);
+		Ssl ssl = new Ssl();
+		if (sslConfig.isEnable()) {
+			ssl.setEnabled(sslConfig.isEnable());
+			ssl.setKeyStore(sslConfig.getKeyStore());
+			ssl.setKeyStoreType(sslConfig.getKeyStoreType());
+			ssl.setKeyStorePassword(sslConfig.getKeyStorePassword());
+			ssl.setKeyAlias(sslConfig.getKeyAlias());
+			ssl.setKeyPassword(sslConfig.getKeyPassword());
+			ssl.setEnabledProtocols(sslConfig.getEnabledProtocols().split(","));
+		}
+		return ssl;
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		Configurations configurations = xmlReader.loadConfigFile();
+		Map<Permission, Object> permissions = configurations.getConfigurations(Permission.DB.getId());
+		DBConfig dbConfig = (DBConfig) permissions.get(Permission.DB);
+		DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+		dataSourceBuilder
+				.url("jdbc:mysql://" + dbConfig.getIp() + ":" + dbConfig.getPort() + "/" + dbConfig.getDatabase());
+		dataSourceBuilder.username(dbConfig.getUser());
+		dataSourceBuilder.password(dbConfig.getPassword());
+		return dataSourceBuilder.build();
+	}
+
 }
--- src/main/java/demo/config/SSLConfig.java
+++ src/main/java/demo/config/SSLConfig.java
@@ -1,5 +1,8 @@
 package demo.config;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +15,14 @@ import lombok.ToString;
 @Setter
 @ToString
 public class SSLConfig {
+	private boolean enable;
+	@Min(0)
+	@Max(65536)
+	private int port;
+	private String enabledProtocols;
 	private String keyStoreType;
 	private String keyStore;
 	private String keyStorePassword;
 	private String keyAlias;
+	private String keyPassword;
 }
